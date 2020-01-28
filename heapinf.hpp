e7331305fe762e5a2dd2f0edfb3efafd8fba6843
SangameshMOtageri <45513969+SangameshMOtageri@users.noreply.github.com>
2020-01-28 19:29:58 +0530

    Update heapinf.hpp

--- heapinf.hpp
+++ heapinf.hpp
@@ -1,7 +1,13 @@
+#pragma once
 #include<stdio.h>
 #include<iostream>
 #include<string>
+#include "pch.h"
 
 using namespace std;
 
-int heapmainfunc(int val, int index, string cmd);
+extern "C" __declspec(dllexport) int heapmainfunc(int val, int index, string cmd);
+
+extern "C" __declspec(dllexport) int huffman(char* input, unsigned int len);
+
+extern "C" __declspec(dllexport) char testvect(char a);
